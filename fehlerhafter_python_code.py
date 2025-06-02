cb70302694d8e2e7490d1a77974af371eb7fe43c
Saman-Mahdipoor <samanmahdipoor4@gmail.com>
2025-06-02 23:10:10 +0200

    Create fehlerhafter_python_code.py
    commit changes

---
+++ fehlerhafter_python_code.py
@@ -0,0 +1,24 @@
+# fehlerhafter_python_code.py
+
+def addiere_zahlen(a, b):
+    summe = a + b
+    return
+
+def drucke_nachricht():
+    print("Dies ist eine Nachricht"
+    
+def nutzer_eingabe():
+    name = input("Gib deinen Namen ein: ")
+    if name == "admin":
+        print("Willkommen Admin")
+    elif name = "user":  # Syntaxfehler: == statt =
+        print("Willkommen Nutzer")
+    else:
+        print("Willkommen", name)
+
+def nie_genutzt():
+    x = 5 + 6
+
+drucke_nachricht()
+nutzer_eingabe()
+addiere_zahlen(3, 4)
